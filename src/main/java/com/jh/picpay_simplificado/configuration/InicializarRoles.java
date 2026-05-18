abe159b89d39ae0569b3188d5fdf29bf4ecf2245
joão henrique <joaolgg2estudos@gmail.com>
2026-05-18 19:28:53 -0300

    ajustando o nome da classe e os métodos que estavam com 'name'

--- src/main/java/com/jh/picpay_simplificado/configuration/InicializarRoles.java
+++ src/main/java/com/jh/picpay_simplificado/configuration/InicializarRoles.java
@@ -11,22 +11,22 @@ import com.jh.picpay_simplificado.entity.Role;
 import com.jh.picpay_simplificado.repository.RoleRepository;
 
 @Component
-public class CommandLine implements CommandLineRunner{
+public class InicializarRoles implements CommandLineRunner{
 	
 	@Autowired
 	private RoleRepository roleRepository;
 
 	@Override
 	public void run(String... args) throws Exception {
-		Optional<Role> optionalRole = roleRepository.findByName(Role.Value.LOJISTA.name());
+		Optional<Role> optionalRole = roleRepository.findByNome(Role.Value.LOJISTA.name());
 		
 		if(optionalRole.isEmpty()) {
 			Role roleUsuario = Role.builder()
-					.name(Role.Value.USUARIO.name())
+					.nome(Role.Value.USUARIO.name())
 					.build();
 			
 			Role roleLojista = Role.builder()
-					.name(Role.Value.LOJISTA.name())
+					.nome(Role.Value.LOJISTA.name())
 					.build();
 			
 			roleRepository.saveAll(List.of(roleUsuario, roleLojista));
