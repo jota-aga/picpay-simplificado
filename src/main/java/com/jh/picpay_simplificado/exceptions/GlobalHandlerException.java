fb85d0d3eee3340d43f23da6d36fa7491e88756b
joão henrique <joaolgg2estudos@gmail.com>
2026-05-19 17:38:13 -0300

    handler para COnflictExpcetion e MethodArgumentInvalid

--- src/main/java/com/jh/picpay_simplificado/exceptions/GlobalHandlerException.java
+++ src/main/java/com/jh/picpay_simplificado/exceptions/GlobalHandlerException.java
@@ -1,7 +1,11 @@
 package com.jh.picpay_simplificado.exceptions;
 
+import java.util.HashMap;
+import java.util.Map;
+
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
@@ -16,5 +20,21 @@ public class GlobalHandlerException {
 	@ExceptionHandler(NotAuthorizedException.class)
 	public ResponseEntity<?> handlerNotAuthorizedException(NotAuthorizedException ex) {
 		return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body(ex.getMessage());
+	}
+	
+	@ExceptionHandler(ConflictException.class)
+	public ResponseEntity<?> handlerConflictException(ConflictException ex) {
+		return ResponseEntity.status(HttpStatus.CONFLICT).body(ex.getMessage());
+	}
+	
+	@ExceptionHandler(MethodArgumentNotValidException.class)
+	public ResponseEntity<?> handlerMethodArgumentNotValidException(MethodArgumentNotValidException ex) {
+		Map<String, String> errors = new HashMap();
+		
+		ex.getBindingResult()
+			.getFieldErrors()
+			.forEach(error -> errors.put(error.getField(), error.getDefaultMessage()));
+		
+		return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(errors);
 	}
 }
